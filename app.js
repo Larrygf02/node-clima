eecf2e4cf729e0b8a50360f4bf89cdaebe43be06
larrygf02 <raulgf_02@hotmail.com>
2019-04-08 17:54:28 -0500

    Uniendo los servicios

--- app.js
+++ app.js
@@ -13,7 +13,25 @@ const argv = require('yargs').options({
 /*lugar.getLugarLatLng(argv.direccion)
     .then(resp => console.log(resp))*/
 
-clima.getClima(40.750000,-74.000000 )
+/*clima.getClima(40.750000,-74.000000 )
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))*/
+
+const getInfo = async (direccion) => {
+    const resp = await lugar.getLugarLatLng(direccion);
+    let info = ''
+    try {
+        const { lat, lon } = resp
+        let temp = await clima.getClima(lat, lon)
+        info = `El clima de ${direccion} es ${temp}`
+    } catch (error) {
+        info = `No se puede determinar el clima de ${direccion}`
+    }
+
+    return info;
+}
+
+getInfo(argv.direccion)
     .then(resp => console.log(resp))
     .catch(err => console.log(err))
 
